e74e7705f759e4660b3e3497250ec75470d7ea31
GeorgiiBubnov <georgii.bubnov.spb@gmail.com>
2024-05-09 13:50:32 +0300

    Add Test

---
+++ src/test/java/DragAndDropTest.java
@@ -0,0 +1,32 @@
+import com.codeborne.selenide.DragAndDropOptions;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class DragAndDropTest extends TestBase {
+
+    @Test
+    void dragAndDropTestVersionOne() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        actions().moveToElement($("#column-a")).clickAndHold().moveToElement($("#column-b")).release()
+                .perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+
+    }
+
+    @Test
+    void dragAndDropTestVersionTwo() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        $("#column-a").dragAndDrop(DragAndDropOptions.to("#column-b"));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+
+    }
+}
